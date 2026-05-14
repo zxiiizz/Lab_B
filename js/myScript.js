4565afc32d2089eda5bcd3434d32539baf3cd5eb
zxiiizz <qaz10787870@gmail.com>
2026-05-14 10:51:54 +0800

    新增彈出視窗

--- js/myScript.js
+++ js/myScript.js
@@ -15,3 +15,16 @@ function openLink(evt, linkName) {
 
 // Click on the first tablink on load
 document.getElementsByClassName("tablink")[0].click();
+
+
+// 新增彈出視窗的程式碼
+var modalElem = document.getElementById("tyModal");
+var spanElem = document.getElementsByClassName("tyClose")[0];
+
+spanElem.onclick = function () {
+    modalElem.style.display = "none";
+}
+
+setTimeout(function () {
+    modalElem.style.display = "block";
+}, 1000);
